951a8e798946844e3a433707e81f347cdec1a7af
spacekimchis <spacekimchis@gmail.com>
2023-02-06 20:18:21 -0800

    wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App subject="subject_text" />);
+	const linkElement = screen.getByText(/learn react/i);
+	expect(linkElement).toBeInTheDocument();
 });
