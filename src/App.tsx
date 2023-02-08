85a18c3e02e163947230c1659b3f9e9722af9095
spacekimchis <spacekimchis@gmail.com>
2023-02-07 19:58:11 -0800

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo';
 
 interface appProps {
-	subject: string;
+	tasks: Array<{id: string, name: string, completed: boolean}>;
 }
 
+/*
+
+interface EnumServiceItem {
+    id: number; label: string; key: any
+}
+
+interface EnumServiceItems extends Array<EnumServiceItem>{}
+
+
+// Option A 
+var result: EnumServiceItem[] = [
+    { id: 0, label: 'CId', key: 'contentId' },
+    { id: 1, label: 'Modified By', key: 'modifiedBy' },
+    { id: 2, label: 'Modified Date', key: 'modified' },
+    { id: 3, label: 'Status', key: 'contentStatusId' },
+    { id: 4, label: 'Status > Type', key: ['contentStatusId', 'contentTypeId'] },
+    { id: 5, label: 'Title', key: 'title' },
+    { id: 6, label: 'Type', key: 'contentTypeId' },
+    { id: 7, label: 'Type > Status', key: ['contentTypeId', 'contentStatusId'] }
+];
+
+*/
+
 function App(props: appProps) {
-	const subject = "React";
-	console.log(props);
+	const taskList = props.tasks?.map((task) =>
+		<Todo
+			name={task.name}
+			completed={task.completed}
+			id={task.id}
+			key={task.id}
+		/>
+	);
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Hello, {props.subject}!!
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="todoapp stack-large">
+			<h1>TodoMatic</h1>
+			<form>
+				<h2 className="label-wrapper">
+					<label htmlFor="new-todo-input" className="label__lg">
+						What needs to be done?
+					</label>
+				</h2>
+				<input
+					type="text"
+					id="new-todo-input"
+					className="input input__lg"
+					name="text"
+					autoComplete="off"
+				/>
+				<button type="submit" className="btn btn__primary btn__lg">
+					Add
+				</button>
+			</form>
+			<div className="filters btn-group stack-exception">
+				<button type="button" className="btn toggle-btn" aria-pressed="true">
+					<span className="visually-hidden">Show </span>
+				<span>all</span>
+					<span className="visually-hidden"> tasks</span>
+				</button>
+				<button type="button" className="btn toggle-btn" aria-pressed="false">
+					<span className="visually-hidden">Show </span>
+				<span>Active</span>
+					<span className="visually-hidden"> tasks</span>
+				</button>
+				<button type="button" className="btn toggle-btn" aria-pressed="false">
+					<span className="visually-hidden">Show </span>
+				<span>Completed</span>
+					<span className="visually-hidden"> tasks</span>
+				</button>
+			</div>
+			<h2 id="list-heading">3 tasks remaining</h2>
+			<ul
+				role="list"
+				className="todo-list stack-large stack-exception"
+				aria-labelledby="list-heading">
+				{
+					taskList
+				}
+			</ul>
 		</div>
 	);
 }
+
 export default App;
